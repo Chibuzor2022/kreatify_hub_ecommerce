a842c83bd0fc6dfad110b2716c8834668f1e276f
Chibuzor2022 <incovenant@hotmail.com>
2025-06-25 08:15:45 +0100

    npmbuild2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,30 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-import tailwindcss from '@tailwindcss/vite';
+// import react from "@vitejs/plugin-react";
+// import { defineConfig } from "vite";
+// import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
+// export default defineConfig({
+// 	plugins: [react(), tailwindcss(),
+// 	],
+// 	server: {
+// 		proxy: {
+// 			"/api": "http://localhost:5000", // forward requests to backend
+// 		},
+// 	},
+//  build: {
+//     outDir: '../dist', // Temporary build directory
+//     emptyOutDir: true
+//   }
+// });
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// Remove tailwind plugin temporarily for debugging
 export default defineConfig({
-	plugins: [react(), tailwindcss(),
-	],
-	server: {
-		proxy: {
-			"/api": "http://localhost:5000", // forward requests to backend
-		},
-	},
- build: {
-    outDir: '../dist', // Temporary build directory
+  plugins: [react()], // Only react plugin for now
+  build: {
+    outDir: '../backend/public',
     emptyOutDir: true
   }
-});
-
+})
