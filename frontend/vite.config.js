e83fda42bbd94e1d420ca43057ded486f0506eef
Chibuzor2022 <incovenant@hotmail.com>
2025-06-25 13:16:58 +0100

    tailwind

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,23 +1,21 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-// import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from '@tailwindcss/vite';
 
-// // https://vite.dev/config/
-// export default defineConfig({
-// 	plugins: [react(), tailwindcss(),
-// 	],
-// 	server: {
-// 		proxy: {
-// 			"/api": "http://localhost:5000", // forward requests to backend
-// 		},
-// 	},
-//  build: {
-//     outDir: '../dist', // Temporary build directory
-//     emptyOutDir: true
-//   }
-// });
-
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react(), tailwindcss(),
+	],
+	server: {
+		proxy: {
+			"/api": "http://localhost:5000", // forward requests to backend
+		},
+	},
+ build: {
+    outDir: '../dist', // Temporary build directory
+    emptyOutDir: true
+  }
 });
+
+
 
