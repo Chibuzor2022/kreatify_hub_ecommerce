b8ff295380f665678e7ed3c1c35e56451d8f2283
Chibuzor2022 <incovenant@hotmail.com>
2025-06-26 04:08:11 +0100

    build

--- backend/server.js
+++ backend/server.js
@@ -43,14 +43,14 @@ app.use(express.urlencoded({ extended: true }));
 
 
 // Add this before your API routes
-// const __dirname = path.resolve();
+const __dirname = path.resolve();
 
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '/frontend/dist')));
-//   app.get('*', (req, res) =>
-//     res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
-//   );
-// }
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+}
 
 // Routes
 app.use("/api/users", userRoutes);
